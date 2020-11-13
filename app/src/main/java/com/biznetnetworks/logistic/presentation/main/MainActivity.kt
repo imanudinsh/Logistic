6badc5ea26c734c865eeb337d6cf47a76bc0d201
imanudin_sholeh <imanudin_sholeh@biznetnetworks.com>
2020-11-13 17:46:41 +0700

    Initial commit

---
+++ app/src/main/java/com/biznetnetworks/logistic/presentation/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.biznetnetworks.logistic.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.biznetnetworks.logistic.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
